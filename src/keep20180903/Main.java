d65546be67eafd3ce5baaf463309a9de219a5c5e
Watcher <lanrengufeng@163.com>
2018-09-03 21:00:58 +0800

    add keep

---
+++ src/keep20180903/Main.java
@@ -0,0 +1,13 @@
+package keep20180903;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String s
+
+
+        in.close();
+    }
+}
---
+++ src/keep20180903/Main2.java
@@ -0,0 +1,13 @@
+package keep20180903;
+
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+
+
+        in.close();
+    }
+}
